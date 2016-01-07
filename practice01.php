02a9a911a7e1cab69cc0e64959decd011ab90329
gaocun <ichitakamura@hotmail.com>
2016-01-07 09:51:52 +0000

    練習問題１

---
+++ practice01.php
@@ -0,0 +1,18 @@
+<?php
+
+$familyName = '高村';
+
+$firstName = '一郎';
+
+$age = '40';
+
+$five = 5;
+
+$FIVE = '5';
+
+echo $familyName . $firstName . 'の年齢は' . $age . '歳です';
+
+echo '<br>';
+
+var_dump($five);
+var_dump($FIVE);
